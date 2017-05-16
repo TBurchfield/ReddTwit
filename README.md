08854bc3df8e2423e71b054ca2b1251b388e3cb7
Selene Cadmus <aasava@protonmail.com>
2017-05-16 13:04:53 -0700

    fix readme typo + start interfacing reddit api

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## Overview
 
-This bot will eventually post synopses of twitter feeds occasionally to twitter.
+This bot will eventually post synopses of twitter feeds occasionally to reddit.
 
 Work in progress.
--- update.py
+++ update.py
@@ -3,19 +3,32 @@
 #Will scrape a list of twitter users, and make reddit posts about their tweets
 
 import os
+import sys
 import json
 import praw
 
 
 try:
-	client_id = os.environ["RedditClientID"]
-	client_secret = os.environ["RedditSecret"]
-	username = os.environ["RedditUsername"]
-	client_id = os.environ["RedditPassword"]
+  cli_id = os.environ["RedditClientID"]
+  cli_sec = os.environ["RedditSecret"]
+  user = os.environ["RedditUsername"]
+  passwd = os.environ["RedditPassword"]
+  agent = os.environ["RedditAgent"]
+
 except KeyError:
-	print ""
-	print "Error:"
-	print ""
-	print "Please set RedditClientID, RedditSecret,"
-	print "RedditPassword, and RedditUsername in your environment variables."
-	print ""
+  print ""
+  print "Error:"
+  print ""
+  print "Please set RedditClientID, RedditSecret,"
+  print "RedditPassword, RedditAgent, and RedditUsername"
+  print "in your environment variables."
+  print ""
+  sys.exit(1);
+
+reddit = praw.Reddit(client_id=cli_id,
+                     client_secret=cli_sec,
+                     username=user,
+                     password=passwd,
+                     user_agent=agent)
+
+print reddit.user.me()
