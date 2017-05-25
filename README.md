538a7cd16f605e084453b8ddc1484cd48af91efe
Tim Burchfield <tburchfi@nd.edu>
2017-05-24 21:46:39 -0700

    Implemented most of update.py. The script takes two arguments, subreddit, and twitter handle. Added config file functionality to only post new tweets. These files take the form ".handle-subreddit" in the same directory as update.py, and contain the id of the most recent tweet of said handle and subreddit. No empty post is made if no tweets are returned, for instance, if the user has not tweeted since the script has last run. The readme also has been updated with guidance on setting up the needed credentials in the environment variables, and running the script.

--- README.md
+++ README.md
@@ -1,5 +1,26 @@
 ## Overview
 
-This bot will eventually post synopses of twitter feeds occasionally to reddit.
-
+This bot posts synopses of twitter feeds to reddit.
 Work in progress.
+
+## Setup
+
+First, you must get your api credentials from reddit and twitter.
+
+Make sure you have an account on both, then follow the instructions [here](http://progur.com/2016/09/how-to-create-reddit-bot-using-praw4.html) under **Registering the Bot** for reddit.  You need to do a similar process [on Twitter](https://apps.twitter.com/), just make sure you also get an Access Token and Token Secret.
+
+Through this process, you will get a Client ID and Secret from Reddit, and your Twitter Key, Secret, Token, and Token Secret.
+
+Set RedditClientID, RedditSecret, RedditUsername, RedditPassword, TwitterKey, TwitterSecret, TwitterToken, and TwitterTokenSecret appropriately in your environment variables.  Lastly, set RedditAgent in your environment variables.  It doesn't seem to have exact specifications, but it's best to include your username, and a little bit about your bot.  Something like `/u/myusername bot to summarize twitter feeds` works fine.
+
+It's easiest to export these in your .bashrc.
+
+## How to use
+
+In order to make a single post (not scheduled), just run `./update.py subredditname twitterhandle`.
+
+The script automatically posts only tweets you have not yet posted to that subreddit, for that particular user.
+
+*Note: this is not done in a particularly robust way.  The id of the most recent tweet fetched is simply saved to a file in the current directory identifying the user and subreddit, and only tweets newer than that id are fetched.  This could fail if you try and post too often to reddit, since an id would be saved that did not post.  This could also fail if the user specified has tweeted more than 200 times since the script was last run.  This may be improved later.*
+
+To make regular posts, simply set a cronjob to run at the interval you specify.
--- update.py
+++ update.py
@@ -1,43 +1,107 @@
 #!/usr/bin/env python2.7
-
 #Will scrape a list of twitter users, and make reddit posts about their tweets
-
 import os
 import sys
-import json
 import praw
-
+import twitter
 
 try:
-  cli_id = os.environ["RedditClientID"]
-  cli_sec = os.environ["RedditSecret"]
-  user = os.environ["RedditUsername"]
-  passwd = os.environ["RedditPassword"]
-  agent = os.environ["RedditAgent"]
+    Rcli_id  = os.environ["RedditClientID"]
+    Rcli_sec = os.environ["RedditSecret"]
+    Ruser    = os.environ["RedditUsername"]
+    Rpasswd  = os.environ["RedditPassword"]
+    Ragent   = os.environ["RedditAgent"]
+    TKey     = os.environ["TwitterKey"]
+    TSecret  = os.environ["TwitterSecret"]
+    TTok     = os.environ["TwitterToken"]
+    TTokSec  = os.environ["TwitterTokenSecret"]
 
 except KeyError:
-  print ""
-  print "Error:"
-  print ""
-  print "Please set RedditClientID, RedditSecret,"
-  print "RedditPassword, RedditAgent, and RedditUsername"
-  print "in your environment variables."
-  print ""
-  sys.exit(1);
+    print ""
+    print "Please set ALL of:"
+    print "----------------------------------"
+    print "RedditClientID        RedditSecret"
+    print "RedditAgent         RedditPassword"
+    print "RedditUsername          TwitterKey"
+    print "TwitterSecret         TwitterToken"
+    print "TwitterTokenSecret"
+    print "----------------------------------"
+    print "in your environment variables."
+    print ""
+    sys.exit(1);
 
+def get_statuses(name, since, usesince, twitterapi):
+    # fetches statuses, returns list of status instances
+    if usesince: #we have a most recent
+        statuses = twitterapi.GetUserTimeline(
+                   screen_name=name, include_rts=False, \
+                   exclude_replies=True, since_id=since, count=200)
+    else:
+        statuses = twitterapi.GetUserTimeline(
+                   screen_name=name, include_rts=False, \
+                   exclude_replies=True, count=200)
+    return statuses
 
-def gettweets():
-  #will eventually get tweets from interwebs
-  return ("Wow \n \n"
-          "look at *all* \n \n"
-          "mah strings")
+def header(name):
+    ret = "{}'s tweets\n\n".format(name)
+    ret += "----\n\n"
+    return ret
 
+def footer():
+    ret = "----\n\n"
+    ret += "Boop.  I am a bot made with ReddTwit, the source code for which can be found [here](https://github.com/TBurchfield/ReddTwit)."
+    return ret
 
-reddit = praw.Reddit(client_id=cli_id,
-                     client_secret=cli_sec,
-                     username=user,
-                     password=passwd,
-                     user_agent=agent)
+def status_to_str(name, status):
+    # Called by statuses_to_str, just returns a nicely formatted string
+    # representing reddit markdown for a tweet.
+    ret = u">{}\n\n".format(status.text)
+    ret += "[link](https://twitter.com/{}/status/{})\n\n".format(name, status.id)
+    ret += "&nbsp;\n\n"
+    return ret
 
-print reddit.user.me()
-reddit.subreddit('prawtestinggrounds').submit(title="Tweets of {}".format("placeholder"),selftext=gettweets())
+def statuses_to_str(name, statuses):
+    # takes list of status instances, and converts to string by calling status_to_str
+    post_string = header(name)
+    for status in statuses:
+        post_string += status_to_str(name, status)
+    return post_string
+
+def config(name, subreddit):
+    #fetch config, if it exists
+    usesince = False
+    since = 0
+    filename = ".{}-{}".format(name,subreddit)
+    if os.path.isfile(filename):
+        usesince = True;
+        with open(filename, "r") as f:
+            since = f.readline().strip()
+    return [since, usesince]
+
+def update_config(name, subreddit, since):
+    filename = ".{}-{}".format(name,subreddit)
+    with open(filename,"w") as f:
+        f.write(since+"\n")
+
+if __name__ == "__main__": # just in case anyone imports this
+    subreddit = sys.argv[1]  # which subreddit to post to
+    handle = sys.argv[2]     # which user to scrape
+    redditapi = praw.Reddit(client_id=Rcli_id,
+                         client_secret=Rcli_sec,
+                         username=Ruser,
+                         password=Rpasswd,
+                         user_agent=Ragent)
+  
+    twitterapi = twitter.Api(consumer_key=TKey,
+                         consumer_secret=TSecret,
+                         access_token_key=TTok,
+                         access_token_secret=TTokSec)
+  
+    [since, usesince] = config(handle, subreddit)
+    statuses = get_statuses(handle,since,usesince,twitterapi)
+    if len(statuses) > 0:
+        update_config(handle, subreddit, statuses[0].id_str) #just in case id overflows
+        mypost =  statuses_to_str(handle,statuses)
+        redditapi.subreddit(subreddit).submit(title="Tweets of {}".format(handle),selftext=mypost)
+    else:
+        print "No new posts from {}.".format(handle)
